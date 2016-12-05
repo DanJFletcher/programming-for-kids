0ba9141e9fb7a724ff6fceaeacc04476381fbc22
Dan Fletcher <dannyjfletcher@outlook.com>
2016-12-05 02:32:00 +0000

    Chore: remove uneeded comments

--- src/Game/index.jsx
+++ src/Game/index.jsx
@@ -1,10 +1,5 @@
-// Dependancies
 import React, { Component } from 'react';
-
-// Styles
 import './Game.css';
-
-// Components
 import GameBoard from './GameBoard';
 import GameControls from './GameControls';
 
